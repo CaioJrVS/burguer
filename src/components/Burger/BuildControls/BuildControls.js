102e152c45918867e4c42f7200da47d06ff9a86f
Caio <caio@cjvs.note>
2020-08-17 16:53:45 -0300

    add: OrderSumary, Logo, Navigation, UI

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -23,6 +23,9 @@ const buildControls= (props)=>(
 				disabled={props.disabled[item.type]}
 			/>
 		))}
+		<button className={classes.OrderButton} disabled={!props.purchasable} onClick={props.ordered}>
+			ORDER NOW
+		</button>
 	</div>		
 );
 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES={
 	salad:0.5,
@@ -20,7 +22,17 @@ class BurgerBuilder extends Component{
 			cheese:0,
 			meat:0
 		},
-		totalPrice:4
+		totalPrice:4,
+		purchasable: false,
+		purchasing: false
+	}
+
+	updatePurchaseState(ingredients){
+		const sum = Object.keys(ingredients)
+			.map((igKey)=>{
+				return ingredients[igKey];
+			}).reduce((sum,k)=>sum+k,0)
+		this.setState({purchasable: sum > 0})
 	}
 
 	addIngredientHandler = (type)=>{
@@ -35,6 +47,7 @@ class BurgerBuilder extends Component{
 			ingredients:updatedIngredients,
 			totalPrice:newPrice
 		})
+		this.updatePurchaseState(updatedIngredients)
 	}
 
 	removeIngredient=(type)=>{
@@ -50,6 +63,19 @@ class BurgerBuilder extends Component{
 			ingredients:updatedIngredients,
 			totalPrice:newPrice
 		})
+		this.updatePurchaseState(updatedIngredients)
+	}
+
+	purchaseHandler = ()=>{
+		this.setState({purchasing:true})
+	}
+
+	purchaseCancelHandler = ()=>{
+		this.setState({purchasing:false});
+	}
+
+	purchaseContinueHandler = ()=> {
+		alert('You Continue!');
 	}
 
 	render(){
@@ -61,12 +87,22 @@ class BurgerBuilder extends Component{
 
 		return(
 			<Aux>
+				<Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler} >
+					<OrderSummary 
+						ingredients={this.state.ingredients}
+						purchaseCanceled={this.purchaseCancelHandler}
+						purchaseContinued={this.purchaseContinueHandler}
+						price = {this.state.totalPrice}
+					/>
+				</Modal>
 				<Burger ingredients={this.state.ingredients}/>
 				<BuildControls 
 					ingredientAdded={this.addIngredientHandler} 
 					ingredientRemoved={this.removeIngredient} 
 					disabled={disabledInfo}
 					price={this.state.totalPrice}
+					purchasable={this.state.purchasable}
+					ordered={this.purchaseHandler}
 				/>
 			</Aux>
 		);
